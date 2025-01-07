bc8d45a53d7e2150c5bc5a6be8948fd2a1109dd6
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-01-07 11:13:42 -0800

    Initial Commit

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,50 @@
+let firstConstant = 10
+
+var secondVariable = 20
+secondVariable = 10
+
+var myFirstString = "Hello World!"
+
+var firstDouble = 2.0
+
+var myfirstBool = true
+
+var otherString: String = "Hello"
+var otherInt: Int = 10
+var otherDouble: Double = 2.0
+var otherBool: Bool = true
+
+
+var n1 = 10
+var n2 = 20
+
+var sum = n1 + n2
+var substrate = n1 - n2
+var multiplication = n1 * n2
+var division1 = Double(n1) / Double(n2)
+
+var n3: Double = 10
+var n4: Double = 20
+
+var division2 = n3 / n4
+
+var s1 = "Hello"
+var s2 = "World"
+
+var concatenation_1 = s1 + " " + s2
+var concatenation_2 = ("\(s1) \(s2)")
+
+var b1 = true
+var b2 = false
+
+var or_1 = b1 || b2
+var and = b1 && b2
+var not = !b1
+
+let name = "Jonathan"
+let greeting = "Hello \(name). This is the Swift course!"
+print(greeting)
+
+// Challenge: Number of chars in the variable name
+let messageNameLength = "The count of characters in the variable name is: \(name.count)"
+print(messageNameLength)
