c1ea63e824cb38cd382d863aa26ee0b15770ba6a
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-01-20 13:13:01 -0800

    Loops: for

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -19,3 +19,56 @@ while guess != answer {
     }
     
 }
+
+for number in 1...10 {
+    print("The number is: \(number)")
+}
+
+let nameList = ["Alice", "Bob", "Charlie"]
+let nametoSearch = "charlie"
+
+var searchResult = ""
+
+for name in nameList {
+    if name.lowercased() == nametoSearch.lowercased() {
+        searchResult = "I've found \(name)"
+    }
+}
+
+if searchResult.isEmpty {
+    print("I didn't found anybody named: \(nametoSearch)")
+} else {
+    print(searchResult)
+}
+
+let text = "Hello, my name is Jonathan"
+for letter in text {
+    print(letter)
+}
+
+// Challenge
+/* TODO: Exercise
+    Write a function that executes a loop through a String to count how many vowels contains.
+ 
+    Tip: You can use this String "aeiouáéíóúAEIOUÁÉÍÓÚ" as a variable to determine the Spanish vowels.
+    you can use the "contains" function for the Strings
+ */
+
+func countVowels(text: String) -> Int {
+    let vowelsInSpanish = "aeiouáéíóúAEIOUÁÉÍÓÚ"
+    var vowelCounter: Int = 0
+    
+    for character in text {
+        if vowelsInSpanish.contains(character) {
+            vowelCounter += 1
+        }
+    }
+    return vowelCounter
+}
+
+
+let textToTest = "Hello, my name is Jonathan"
+let vowelcounting = countVowels(text: textToTest)
+
+print("The text contains \"\(vowelcounting)\" vowels.")
+
