586218e6cc8dad9bc25fe10857c052f9f92029d1
Jonathan_Meixueiro <lav.jonathanom@gmail.com>
2025-01-29 10:26:44 -0800

    Testing students manager service

--- System Students Manager/SystemManager.playground/Contents.swift
+++ System Students Manager/SystemManager.playground/Contents.swift
@@ -1,11 +1,25 @@
 let math = Subject(name: "Math", teacherName: "Mr. Smith", level: SubjectLevel.intermediate)
 let spanish = Subject(name: "Spanish", teacherName: "Mrs. Meixueiro", level: .basic)
+let biology = Subject(name: "Biology", teacherName: "Peter Parker", level: .intermediate)
+let economy = Subject(name: "Economy", teacherName: "Harry Osborn", level: .intermediate)
 
-let s1 = Student(name: "John", age: 30, email: "new@email.com", subjects: [math, spanish], grades: [70.0, 80.0])
-let s2 = AdvancedStudent(name: "Edward", age: 28, email: "advanced@email.com", subjects: [math, spanish], grades: [90.0, 60.0], extraPoints: 10.0)
+let s1 = Student(name: "John", age: 30, email: "new@email.com", subjects: [], grades: [])
+let s2 = AdvancedStudent(name: "Edward", age: 28, email: "advanced@email.com", subjects: [], grades: [], extraPoints: 10.0)
 
-let desc1 = s1.describe()
-let desc2 = s2.describe()
+let service = StudentsManager(students: [])
+service.insertStudent(s1)
+service.insertStudent(s2)
 
-print(desc1)
-print(desc2)
+service.assignSubjectToStudent(subject: math, grade: 80, student: s1)
+service.assignSubjectToStudent(subject: spanish, grade: 70, student: s1)
+service.assignSubjectToStudent(subject: biology, grade: 90, student: s1)
+
+service.assignSubjectToStudent(subject: math, grade: 90, student: s2)
+service.assignSubjectToStudent(subject: spanish, grade: 60, student: s2)
+service.assignSubjectToStudent(subject: economy, grade: 60, student: s2)
+
+service.generateStudentsReport()
+
+
+let average = service.getAverages()
+let subjects = service.getCoursedSubjects()
